27b524e383c9620fff87aff002fe8fe4f3150bbc
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-10-16 18:20:42 -0400

    override

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class EfficientWordMarkov extends BaseWordMarkov{
-	Map<WordGram, ArrayList<String>> myMap;
+	private Map<WordGram, ArrayList<String>> myMap;
 	
 	public EfficientWordMarkov(int order) {
 		super(order);
@@ -17,7 +17,19 @@ public class EfficientWordMarkov extends BaseWordMarkov{
 		this(2);
 	}
 	
-//	@Override
+	@Override
+	public void setTraining(String text) {
+		myWords = text.split("\\s+");
+		for (int i = 0; i < myWords.length; i++) {
+			WordGram wg = new WordGram(myWords, i, myOrder);
+			if (! myMap.containsKey(wg)) {
+				myMap.put(wg, new ArrayList<String>());
+			}
+			myMap.put(wg, super.getFollows(wg));
+		}
+	}
+	
+	
 	public void setTraining(String[] words) {
 		myWords = words;
 		for (int i = 0; i < words.length; i++) {
