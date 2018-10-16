cc64cbf313ed323a789ce76011095539daba094d
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2018-10-16 13:20:26 -0400

    initial efficient wordmarkov

---
+++ src/EfficientWordMarkov.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+public class EfficientWordMarkov extends BaseWordMarkov{
+	Map<WordGram, ArrayList<String>> myMap;
+	
+	public EfficientWordMarkov(int order) {
+		super(order);
+		myRandom = new Random(RANDOM_SEED);
+		myMap = new HashMap<>();
+	}
+	
+	public EfficientWordMarkov() {
+		this(2);
+	}
+	
+//	@Override
+	public void setTraining(String[] words) {
+		myMap.clear();
+		myWords = words;
+		for (int i = 0; i < words.length; i++) {
+			WordGram wg = new WordGram(words, i, myOrder);
+			if (! myMap.containsKey(wg)) {
+				myMap.put(wg, new ArrayList<String>());
+			}
+			myMap.put(wg, super.getFollows(wg));
+		}
+	}
+	
+	@Override
+	public ArrayList<String> getFollows(WordGram wg) {
+		if (! myMap.containsKey(wg)) {
+			throw new NoSuchElementException(wg + " not in map");
+		}
+		return myMap.get(wg);
+	}
+}
